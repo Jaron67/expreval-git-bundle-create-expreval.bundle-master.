5426a3e59066a4f88d1c17aa740031be7c5703bb
Jaron67 <jaronsaw13@gmail.com>
2024-06-16 12:46:25 +0200

    Update test_expreval.py

--- test_expreval.py
+++ test_expreval.py
@@ -1,16 +1,7 @@
 from expreval import ExpressionEvaluator
 
-def test_expression_evaluator():
-    evaluator = ExpressionEvaluator()
-
-    assert evaluator.parse("3 + 4 * 2 / ( 1 - 5 )") == "3 4 2 * 1 5 - / +"
-    assert evaluator.evaluate("3 4 2 * 1 5 - / +") == 1
-
-    evaluator = ExpressionEvaluator(invert_precedence=True)
-    assert evaluator.parse("3 + 4 * 2 / ( 1 - 5 )") == "3 4 + 2 * 1 5 - /"
-    assert evaluator.evaluate("3 4 + 2 * 1 5 - /") == -5
-
-if __name__ == "__main__":
-    test_expression_evaluator()
-    print("test_expreval.py passed")
-
+expr = "3 + 5 * 2"
+expr_eval = ExpressionEvaluator()
+rpn = expr_eval.parse(expr)
+val = expr_eval.evaluate(rpn)
+print(f"expr: {expr}\n  rpn: {rpn}\n  val: {val}")
