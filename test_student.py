a1477f3c16e8eaf46616b6d9fcdb71597dae71c2
Jaron67 <jaronsaw13@gmail.com>
2024-06-16 12:47:04 +0200

    Update test_student.py

--- test_student.py
+++ test_student.py
@@ -1,9 +1,9 @@
 import student
 
 def test_student_info():
-    assert student.matriculation_number() == '1234567'
-    assert student.first_name() == 'John'
-    assert student.last_name() == 'Doe'
+    assert student.matriculation_number() == '00152102'
+    assert student.first_name() == 'Jaron'
+    assert student.last_name() == 'Saw'
 
 if __name__ == "__main__":
     test_student_info()
